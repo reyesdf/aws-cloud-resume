7745a3027de33e0878ce9c19d8a73cecafceb46c
Dennis Joseph <reyes.dennisjoseph@gmail.com>
2025-02-25 20:56:35 +0800

    trying to migrate state to remote

--- main.tf
+++ main.tf
@@ -1,4 +1,10 @@
 terraform {
+    cloud { 
+      organization = "jocodes-learning" 
+      workspaces { 
+        name = "learning-terraform" 
+      }
+    }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -8,13 +14,13 @@ terraform {
 }
 
 provider "aws" {
-  region = "ap-southeast-1"
+  region = "ap-southeast-1a"
 }
 
 resource "aws_instance" "terraform-aws-instance" { // resource type, resource name
   ami = "ami-0672fd5b9210aa093" //ami image for UBUNTU 24.04 LTS in southeast-1 region
   instance_type = "t2.micro"
   tags = {
-  Name = "terraform-aws-instance"
+  Name = var.instance_tag
   }
 }
